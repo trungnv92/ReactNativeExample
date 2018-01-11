4807923d8a24271aebe1edac7d1744ef786e1f16
trungvnimm <trungnv.vnimm@gmail.com>
2018-01-11 09:26:11 +0700

    update asyncstorage

--- components/AsyncStg.js
+++ components/AsyncStg.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import { View, TouchableOpacity, AsyncStorage} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, TouchableOpacity, AsyncStorage, StyleSheet} from 'react-native';
 
 export default class AsyncStg extends Component {
 
-    save = async()=>{
+    save = async() => {
         try {
-            await AsyncStorage.setItem('@username:key', 'trung');
+            await AsyncStorage.setItem('@username:key', 'Nguyen Van Trung');
         } catch (error) {
             console.log(error);
         }
-        
+
+    }
+
+    getme = async() => {
+        try {
+            var value = await AsyncStorage.getItem('@username:key');
+            console.log("Get Data: " + value);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render() {
         return (
             <View style={styles.wapper}>
-                <TouchableOpacity style={styles.button} onPress={()=>this.save()}>
+                <TouchableOpacity style={styles.button} onPress={() => this.save()}>
                     <Text>Save Me!!!</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={() => this.getme()}>
                     <Text>Get Me!!!</Text>
                 </TouchableOpacity>
-                
+
             </View>
         )
     }
@@ -30,11 +39,16 @@ export default class AsyncStg extends Component {
 const styles = StyleSheet.create({
     wapper: {
         flex: 1,
-        backgroundColor: 'gray',
-        justifyContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     button: {
-        backgroundColor: 'blue',
-        justifyContent: 'center'
+        backgroundColor: 'yellow',
+        justifyContent: 'center',
+        width: 200,
+        height: 50,
+        alignItems: 'center',
+        borderColor: 'pink',
+        borderWidth: 2
     }
 })
